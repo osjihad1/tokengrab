fc539665e224776319d2dc211c935201638b1a29
osjihad1 <120362880+osjihad1@users.noreply.github.com>
2026-05-18 21:33:13 +0600

    Update server.py

--- server.py
+++ server.py
@@ -126,5 +126,5 @@ if __name__ == '__main__':
     else:
         print("[*] No previous tokens found in volume. Standing by...")
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
